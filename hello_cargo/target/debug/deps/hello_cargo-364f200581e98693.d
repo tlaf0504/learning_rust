b1404b7e330e3564c3d64edb70a1d4d2ecd4bed3
tlaf0504 <tobias.lafer@eclipso.at>
2021-06-20 17:12:08 +0200

    Hello Cargo.

---
+++ hello_cargo/target/debug/deps/hello_cargo-364f200581e98693.d
@@ -0,0 +1,5 @@
+/run/media/tobiaslafer/shared/learning_rust/hello_cargo/target/debug/deps/hello_cargo-364f200581e98693: src/main.rs
+
+/run/media/tobiaslafer/shared/learning_rust/hello_cargo/target/debug/deps/hello_cargo-364f200581e98693.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/run/media/tobiaslafer/shared/learning_rust/hello_cargo/target/debug/hello_cargo: /run/media/tobiaslafer/shared/learning_rust/hello_cargo/src/main.rs
